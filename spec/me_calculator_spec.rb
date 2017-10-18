7f0194c69126d95ffa3dac310616bac832e7cb43
a170937 <tcuong1829@gmail.com>
2017-10-18 16:01:51 +0900

    update

--- spec/me_calculator_spec.rb
+++ spec/me_calculator_spec.rb
@@ -6,7 +6,7 @@ describe MeCalculator do
 		context "given an empty me" do
 			it "returns zero" do
         sleep(600)
-				expect(MeCalculator.add("")).to eql(1)
+				expect(MeCalculator.add("")).to eql(0)
       end
 		end
 	end
